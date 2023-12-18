767d69467ce18ee0ad2066983f4e7a78f58b2523
0VRBOT9 <92735193+0VRBOT9@users.noreply.github.com>
2023-12-18 07:07:26 +0530

    datasets.py

---
+++ datasets.py
@@ -0,0 +1,61 @@
+import os
+from torchvision import datasets, transforms
+
+# Assuming these constants and functions are defined elsewhere in your script.
+from timm.data.constants import \
+    IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
+from timm.data import create_transform
+from sampler import MultiScaleImageFolder
+
+def build_dataset(is_train, args):
+    transform = build_transform(is_train, args)
+
+    print("Transform = ")
+    if isinstance(transform, tuple):
+        for trans in transform:
+            print(" - - - - - - - - - - ")
+            for t in trans.transforms:
+                print(t)
+    else:
+        for t in transform.transforms:
+            print(t)
+    print("---------------------------")
+
+    if args.data_set == 'CIFAR':
+        # Using CIFAR10 instead of CIFAR100 and setting number of classes to 10
+        dataset = datasets.CIFAR10(args.data_path, train=is_train, transform=transform, download=True)
+        nb_classes = 10
+    elif args.data_set == 'IMNET':
+        print("reading from datapath", args.data_path)
+        root = os.path.join(args.data_path, 'train' if is_train else 'val')
+        if is_train and args.multi_scale_sampler:
+            dataset = MultiScaleImageFolder(root, args)
+        else:
+            dataset = datasets.ImageFolder(root, transform=transform)
+        nb_classes = 1000
+    elif args.data_set == "image_folder":
+        root = args.data_path if is_train else args.eval_data_path
+        dataset = datasets.ImageFolder(root, transform=transform)
+        nb_classes = args.nb_classes
+        assert len(dataset.class_to_idx) == nb_classes
+    else:
+        raise NotImplementedError()
+    print("Number of the class = %d" % nb_classes)
+
+    return dataset, nb_classes
+
+def build_transform(is_train, args):
+    # CIFAR-10 specific transforms
+    if is_train:
+        transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # CIFAR-10 normalization
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # CIFAR-10 normalization
+        ])
+    return transform
